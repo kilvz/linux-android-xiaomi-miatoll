cbaec9e23b03f3a91c0c64adc8708341be6cbbba
Sultan Alsawaf <sultan@kerneltoast.com>
2021-02-06 20:17:09 -0800

    binder: Stub out debug prints by default
    Binder code is very hot, so checking frequently to see if a debug
    message should be printed is a waste of cycles. We're not debugging
    binder, so just stub out the debug prints to compile them out entirely.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -145,24 +145,14 @@ static int binder_set_stop_on_user_error(const char *val,
 module_param_call(stop_on_user_error, binder_set_stop_on_user_error,
 	param_get_int, &binder_stop_on_user_error, 0644);
 
-#ifdef DEBUG
-#define binder_debug(mask, x...) \
-	do { \
-		if (binder_debug_mask & mask) \
-			pr_info_ratelimited(x); \
-	} while (0)
-#else
 static inline void binder_debug(uint32_t mask, const char *fmt, ...)
 {
 }
-#endif
-
-#define binder_user_error(x...) \
-	do { \
-		pr_info_ratelimited(x); \
-		if (binder_stop_on_user_error) \
-			binder_stop_on_user_error = 2; \
-	} while (0)
+static inline void binder_user_error(const char *fmt, ...)
+{
+	if (binder_stop_on_user_error)
+		binder_stop_on_user_error = 2;
+}
 
 #define to_flat_binder_object(hdr) \
 	container_of(hdr, struct flat_binder_object, hdr)
